2cd8c2171d6841cab96ba2a661db097fae3b5295
rod-sousa <rod.sousa899@gmail.com>
2023-01-13 19:27:51 -0300

    Revert "revert last commit"
    This reverts commit ec71914d34fadc0f17529d86751a50e417187318.

--- app/src/main/java/rodsousa/dev/br/noteapp/feature_note/domain/util/NoteOrder.kt
+++
@@ -1,3 +0,0 @@
-package rodsousa.dev.br.noteapp.feature_note.domain.util
-
-sealed class NoteOrder
--- app/src/main/java/rodsousa/dev/br/noteapp/feature_note/domain/util/OrderType.kt
+++
@@ -1,6 +0,0 @@
-package rodsousa.dev.br.noteapp.feature_note.domain.util
-
-sealed class OrderType{
-    object Ascending: OrderType()
-    object Descending: OrderType()
-}
